ba677384b7982284dad2a2a2287fb020ebdba24c
Frankz Condori <frankcondori97@gmail.com>
2022-03-21 18:55:57 -0400

    add react router dom

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-     Hello World
-    </div>
-  );
+	return (
+		<div className='App'>
+			<h1>Hello World</h1>
+      <p>Intro to the Salon website</p>
+		</div>
+	);
 }
 
 export default App;
