a21022890365df471a6729106c25d10a53d8a719
Frankz Condori <frankcondori97@gmail.com>
2022-03-22 18:50:25 -0400

    add image to main page

--- src/component/Main/Main.js
+++ src/component/Main/Main.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import background1 from '../../assets/salon.jpg'
 
 function Main(props) {
     return (
+        <>
         <div>
             <h2>Hello from Main</h2>
+            <img src={background1} alt='Salon with brushes' className='background1'></img>
         </div>
+        </>
     );
 }
 
