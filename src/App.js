ac2548e1887c131a5e400c96ea42916909335490
Frankz Condori <frankcondori97@gmail.com>
2022-03-22 16:19:44 -0400

    components added to app.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 import Nav from './component/Nav/Nav';
 import Main from './component/Main/Main';
 import About from './component/About/About';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
 	return (
 		<div className='App'>
 			<Nav />
-			<Main />
-			<About />
+			<Routes>
+				<Route path='/' element={<Main />} />
+				<Route path='/about' element={<About />} />
+			</Routes>
 		</div>
 	);
 }
