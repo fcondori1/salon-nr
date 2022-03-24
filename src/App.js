2a82dd6610311fc9298a7ffaccb0e36eb3fa10dc
Frankz Condori <frankcondori97@gmail.com>
2022-03-24 18:53:26 -0400

    scaffold out services and connect to App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Nav from './component/Nav/Nav';
 import Main from './component/Main/Main';
 import About from './component/About/About';
 import { Routes, Route } from 'react-router-dom';
+import Services from './component/Services/Services';
 
 function App() {
 	return (
@@ -11,6 +12,7 @@ function App() {
 			<Routes>
 				<Route path='/' element={<Main />} />
 				<Route path='/about' element={<About />} />
+				<Route path='/services' element={<Services />} />
 			</Routes>
 		</div>
 	);
