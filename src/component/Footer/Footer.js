9a2ee55da3466731963f8295748d0f52dfd83bc7
Frankz Condori <frankcondori97@gmail.com>
2022-03-26 12:32:36 -0400

    update and scaffold out components

---
+++ src/component/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer(props) {
+    return (
+        <div>
+            <h2>Hello from Footer</h2>
+        </div>
+    );
+}
+
+export default Footer;
---
+++ src/component/Gallery/Gallery.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Gallery(props) {
+    return (
+        <div>
+            <h2>Hello from Gallery</h2>
+        </div>
+    );
+}
+
+export default Gallery;
