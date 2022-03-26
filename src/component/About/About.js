4e069e7046429158690b44326ed26262fcede666
Frankz Condori <frankcondori97@gmail.com>
2022-03-26 12:46:01 -0400

    blueprint for routes

--- src/component/About/About.js
+++ src/component/About/About.js
@@ -4,6 +4,7 @@ function About(props) {
     return (
         <div>
             <h2>Hello from About</h2>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam similique voluptatum velit nihil harum iusto exercitationem quaerat, sed dicta. Accusamus autem modi corrupti, reiciendis enim dolore nihil eaque. Repudiandae, expedita!</p>
         </div>
     );
 }
