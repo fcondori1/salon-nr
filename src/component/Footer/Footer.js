dea456227539eadd950114ca92c637d25b0bc7aa
Frankz Condori <frankcondori97@gmail.com>
2022-03-28 16:43:12 -0400

    add updated logos and connect to href

--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import './Footer.css';
-import facebook from '../../assets/facebook_logo2.png';
-import instagram from '../../assets/instagram_logo.png';
-import linkedin from '../../assets/linkedin_logo2.png';
+import facebook from '../../assets/face_logo.png';
+import instagram from '../../assets/insta_logo2.png';
+import linkedin from '../../assets/linked_logo.png';
 
 function Footer(props) {
 	return (
 		<div className='footer_logos'>
-			<img src={facebook} alt='facebook logo' className='logo'></img>
-			<img src={instagram} alt='instagram logo' className='logo'></img>
-			<img src={linkedin} alt='linkedin logo' className='logo'></img>
+			<a
+				href='https://www.facebook.com/noelia.romero.311'
+				target='_blank'
+				rel='noreferrer'>
+				<img src={facebook} alt='facebook logo' className='logo'></img>
+			</a>
+			<a
+				href='instagram.com/rjnbeauty?utm_medium=copy_link'
+				target='_blank'
+				rel='noreferrer'>
+				<img src={instagram} alt='instagram logo' className='logo'></img>
+			</a>
+			<a
+				href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+				target='_blank'
+				rel='noreferrer'>
+				<img src={linkedin} alt='linkedin logo' className='logo'></img>
+			</a>
 		</div>
 	);
 }
