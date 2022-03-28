b7981bbfc35ceb2738f727e8449654425c52b066
Frankz Condori <frankcondori97@gmail.com>
2022-03-28 14:05:52 -0400

    connect logos to footer

--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import './Footer.css';
+import facebook from '../../assets/facebook_logo2.png';
+import instagram from '../../assets/instagram_logo.png';
+import linkedin from '../../assets/linkedin_logo2.png';
 
 function Footer(props) {
-    return (
-			<div>
-				<h2>Hello from Footer</h2>
-				<ul>
-					<li>Facebook</li>
-					<li>Instagram</li>
-					<li>LinkedIn</li>
-				</ul>
-			</div>
-		);
+	return (
+		<div className='footer_logos'>
+			<img src={facebook} alt='facebook logo' className='logo'></img>
+			<img src={instagram} alt='instagram logo' className='logo'></img>
+			<img src={linkedin} alt='linkedin logo' className='logo'></img>
+		</div>
+	);
 }
 
 export default Footer;
