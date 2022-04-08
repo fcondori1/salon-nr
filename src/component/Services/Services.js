40405408aa9e2a57383373a4a7cfdeb7e0cfe78c
Frankz Condori <frankcondori97@gmail.com>
2022-04-08 14:29:05 -0400

    add some text

--- src/component/Services/Services.js
+++ src/component/Services/Services.js
@@ -4,6 +4,12 @@ function Services(props) {
 	return (
 		<div className='page'>
 			<h2>Hello from Services</h2>
+			<ul>
+				<li>Hair Styling</li>
+				<li>Hair Cutting</li>
+				<li>Hair Coloring</li>
+				<li>Make-up</li>
+			</ul>
 		</div>
 	);
 }
