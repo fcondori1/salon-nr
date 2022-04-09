5224d6f9b6248ba5fd1de98812fff4abbd7a5e0e
Frankz Condori <frankcondori97@gmail.com>
2022-04-09 13:59:48 -0400

    add boxes in services

--- src/component/Services/Services.js
+++ src/component/Services/Services.js
@@ -1,14 +1,14 @@
 import React from 'react';
+import './Services.css';
 
 function Services(props) {
 	return (
 		<div className='page'>
-			<h2>Hello from Services</h2>
-			<ul>
-				<li>Hair Styling</li>
-				<li>Hair Cutting</li>
-				<li>Hair Coloring</li>
-				<li>Make-up</li>
+			<ul className='service-list'>
+				<li className='service-item'>Hair Styling</li>
+				<li className='service-item'>Hair Cutting</li>
+				<li className='service-item'>Hair Coloring</li>
+				<li className='service-item'>Make-up</li>
 			</ul>
 		</div>
 	);
