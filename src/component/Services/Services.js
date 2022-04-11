00292de4c78fee23e5954dc73ffce7d55a9d608f
Frankz Condori <frankcondori97@gmail.com>
2022-04-11 14:06:35 -0400

    mobile nav update

--- src/component/Services/Services.js
+++ src/component/Services/Services.js
@@ -6,7 +6,7 @@ function Services(props) {
 	return (
 		<div className='page'>
 			<div className='service-list'>
-				<div className='service-item'>Hair Stydivng</div>
+				<div className='service-item'>Hair Styling</div>
 				<div className='service-item'>Hair Cutting</div>
 				<div className='service-item'>Hair Coloring</div>
 				<div className='service-item'>Make-up</div>
