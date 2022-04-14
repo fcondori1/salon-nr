7c34816b3de2bdfc6a22ac743e3c48416b66ada2
Frankz Condori <frankcondori97@gmail.com>
2022-04-14 15:54:32 -0400

    update gallery with images

--- src/component/Gallery/Gallery.js
+++ src/component/Gallery/Gallery.js
@@ -1,21 +1,103 @@
 import React from 'react';
 import './Gallery.css';
+import wig1 from '../../assets/gallery/wig1.JPG'
+import wig1a from '../../assets/gallery/wig1a.JPG';
+import wig2 from '../../assets/gallery/wig2.JPG';
+import wig2a from '../../assets/gallery/wig2a.JPG';
+import wig2b from '../../assets/gallery/wig2b.JPG';
+import wig3 from '../../assets/gallery/wig3.JPG';
+import wig3a from '../../assets/gallery/wig3a.JPG';
+import wig4 from '../../assets/gallery/wig4.JPG';
+import wig5 from '../../assets/gallery/wig5.JPG';
+import wig6 from '../../assets/gallery/wig6.JPG';
+import wig7 from '../../assets/gallery/wig7.JPG';
+
 
 function Gallery(props) {
     return (
 			<div className='page'>
-				<ul>
-					<li>Image 1</li>
-					<li>Image 2</li>
-					<li>Image 3</li>
-					<li>Image 4</li>
-				</ul>
-				<p>
-					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci
-					voluptas, dolorem earum quod sint assumenda dolores optio ipsum illum
-					voluptates, vel officiis magnam ipsa. Eius, officia esse? Dolor, natus
-					eum?
-				</p>
+				<div>
+					<img
+						src={wig1}
+						alt=''
+						className='gallery-img'></img>
+					<img
+						src={wig1a}
+						alt=''
+						className='gallery-img'></img>
+					<img
+						// src={}
+						alt=''
+						className='gallery-img'></img>
+					<img
+						// src={}
+						alt=''
+						className='gallery-img'></img>
+					<img
+						// src={}
+						alt=''
+						className='gallery-img'></img>
+					<img
+						// src={}
+						alt=''
+						className='gallery-img'></img>
+					<img
+						// src={}
+						alt=''
+						className='gallery-img'></img>
+					<img
+						// src={}
+						alt=''
+						className='gallery-img'></img>
+					<img
+						// src={}
+						alt=''
+						className='gallery-img'></img>
+					<img
+						// src={}
+						alt=''
+						className='gallery-img'></img>
+					<img
+						// src={}
+						alt=''
+						className='gallery-img'></img>
+				</div>
+				<div>
+					<img
+						// src={}
+						alt=''
+						className='gallery-img'></img>
+					<img
+						// src={}
+						alt=''
+						className='gallery-img'></img>
+					<img
+						// src={}
+						alt=''
+						className='gallery-img'></img>
+					<img
+						// src={}
+						alt=''
+						className='gallery-img'></img>
+				</div>
+				<div>
+					<img
+						// src={}
+						alt=''
+						className='gallery-img'></img>
+					<img
+						// src={}
+						alt=''
+						className='gallery-img'></img>
+					<img
+						// src={}
+						alt=''
+						className='gallery-img'></img>
+					<img
+						// src={}
+						alt=''
+						className='gallery-img'></img>
+				</div>
 			</div>
 		);
 }
