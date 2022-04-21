7743b622586d8f72392c80b5de43fe8c88f2abd3
Frankz Condori <frankcondori97@gmail.com>
2022-04-21 17:45:14 -0400

    create component and add a loading screen

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import logo from './logo.svg';
 
 import Nav from './component/Nav/Nav';
 import Main from './component/Main/Main';
@@ -6,21 +8,34 @@ import About from './component/About/About';
 import Services from './component/Services/Services';
 import Gallery from './component/Gallery/Gallery';
 import Footer from './component/Footer/Footer';
+import Loading from './component/Loading/Loading';
 
 import './App.css';
 
 function App() {
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		setTimeout(() => setLoading(false), 6000)
+	}, []);
+
 	return (
-		<div className='App'>
-			<Nav />
-			<Routes>
-				<Route path='/' element={<Main />} />
-				<Route path='/about' element={<About />} />
-				<Route path='/services' element={<Services />} />
-				<Route path='/gallery' element={<Gallery />} />
-			</Routes>
-			<Footer />
-		</div>
+		<>
+			{loading === false ? (
+				<div className='App'>
+					<Nav />
+					<Routes>
+						<Route path='/' element={<Main />} />
+						<Route path='/about' element={<About />} />
+						<Route path='/services' element={<Services />} />
+						<Route path='/gallery' element={<Gallery />} />
+					</Routes>
+					<Footer />
+				</div>
+			) : (
+				<Loading />
+			)}
+		</>
 	);
 }
 
---
+++ src/component/Loading/Loading.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Loading(props) {
+    return (
+        <div>
+            hello from logo
+        </div>
+    );
+}
+
+export default Loading;
+
