17114f2dfda3ba19bf7f2540706d2107eda81684
Frankz Condori <frankcondori97@gmail.com>
2022-04-22 14:39:02 -0400

    add loading component to main

--- src/component/Main/Main.js
+++ src/component/Main/Main.js
@@ -1,28 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import background1 from '../../assets/salon.jpg';
 import background2 from '../../assets/hair.jpg';
 // import background3 from '../../assets/salon-tools.jpg';
 import './Main.css';
 import Contact from '../Contact/Contact';
+import Loading from '../Loading/Loading';
 
 function Main(props) {
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		setTimeout(() => setLoading(false), 2000);
+	}, []);
+
 	return (
 		<>
-			<div className='page'>
-				<img
-					src={background2}
-					alt='Salon with brushes'
-					className='background1'></img>
-				<Contact />
-				<img
-					src={background1}
-					alt='Salon with brushes'
-					className='background1'></img>
-				{/* <img
+			{loading === false ? (
+				<div className='page'>
+					<img
+						src={background2}
+						alt='Salon with brushes'
+						className='background1'></img>
+					<Contact />
+					<img
+						src={background1}
+						alt='Salon with brushes'
+						className='background1'></img>
+					{/* <img
 					src={background3}
 					alt='Salon with brushes'
 					className='background1'></img> */}
-			</div>
+				</div>
+			) : (
+				<Loading />
+			)}
 		</>
 	);
 }
