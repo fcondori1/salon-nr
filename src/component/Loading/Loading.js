f5ddd9fc4682d9ea9ba855635da0a64cfe44a3b6
Frankz Condori <frankcondori97@gmail.com>
2022-04-22 12:24:14 -0400

    create loading component

--- src/component/Loading/Loading.js
+++ src/component/Loading/Loading.js
@@ -1,12 +1,30 @@
 import React from 'react';
+import './Loading.css';
 
 function Loading(props) {
-    return (
-        <div>
-            hello from logo
-        </div>
-    );
+	return (
+		<div>
+			<div class='loader'>
+				<div class='loader-inner'>
+					<div class='loader-line-wrap'>
+						<div class='loader-line'></div>
+					</div>
+					<div class='loader-line-wrap'>
+						<div class='loader-line'></div>
+					</div>
+					<div class='loader-line-wrap'>
+						<div class='loader-line'></div>
+					</div>
+					<div class='loader-line-wrap'>
+						<div class='loader-line'></div>
+					</div>
+					<div class='loader-line-wrap'>
+						<div class='loader-line'></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Loading;
-
