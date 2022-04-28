7f4ac7eb011f28deb087d313f5f529763e6e815f
Frankz Condori <frankcondori97@gmail.com>
2022-04-28 12:57:36 -0400

    update slideshow component and add filler images

--- src/component/Slideshow/Slideshow.js
+++ src/component/Slideshow/Slideshow.js
@@ -1,35 +1,47 @@
 import React from 'react';
 import { Slide } from 'react-slideshow-image';
 import 'react-slideshow-image/dist/styles.css';
+import img2 from '../../assets/gallery/img2.JPG';
+import img4 from '../../assets/gallery/img4.JPG';
+import img5 from '../../assets/gallery/img3.JPG';
+import './Slideshow.css';
 
 const slideImages = [
-	{
-		url: 'images/slide_2.jpg',
-		caption: 'Slide 1',
-	},
-	{
-		url: 'images/slide_3.jpg',
-		caption: 'Slide 2',
-	},
-	{
-		url: 'images/slide_4.jpg',
-		caption: 'Slide 3',
-	},
+	'https://react-slideshow.herokuapp.com/images/slide_2.jpg',
+	'https://react-slideshow.herokuapp.com/images/slide_3.jpg',
+	'https://react-slideshow.herokuapp.com/images/slide_4.jpg',
 ];
+
+const properties = {
+	duration: 5000,
+	transitionDuration: 500,
+	infinite: true,
+	indicators: true,
+	arrows: true,
+	pauseOnHover: true,
+	onChange: (oldIndex, newIndex) => {
+		console.log(`slide transition from ${oldIndex} to ${newIndex}`);
+	},
+};
 
 const Slideshow = () => {
 	return (
-		<div className='slide-container'>
-			<Slide>
-				{slideImages.map((slideImage, index) => (
-					<div className='each-slide' key={index}>
-						<div style={{ backgroundImage: `url(${slideImage.url})` }}>
-							<span>{slideImage.caption}</span>
-						</div>
+		<>
+			<h2>SlideShow</h2>
+			<div className='slide-container'>
+				<Slide {...properties}>
+					<div className='each-slide'>
+							<img src={img2} alt='' className='slides'></img>
 					</div>
-				))}
-			</Slide>
-		</div>
+					<div className='each-slide'>
+							<img src={img4} alt='' className='slides'></img>
+					</div>
+					<div className='each-slide'>
+							<img src={img5} alt='' className='slides'></img>
+					</div>
+				</Slide>
+			</div>
+		</>
 	);
 };
 
