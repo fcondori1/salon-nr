5739b2fc1b80a71bae99765f96fdc43bc9a593d3
Frankz Condori <frankcondori97@gmail.com>
2022-05-05 10:54:59 -0400

    rearrange images

--- src/component/Main/Main.js
+++ src/component/Main/Main.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import background1 from '../../assets/salon.jpg';
+// import background1 from '../../assets/salon.jpg';
 import background2 from '../../assets/hair.jpg';
 // import background3 from '../../assets/salon-tools.jpg';
 import './Main.css';
@@ -24,10 +24,10 @@ function Main(props) {
 						className='background1'></img>
 					<Slideshow />
 					<Contact />
-					<img
+					{/* <img
 						src={background1}
 						alt='Salon with brushes'
-						className='background1'></img>
+						className='background1'></img> */}
 					{/* <img
 					src={background3}
 					alt='Salon with brushes'
